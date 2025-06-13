e638793be915a2fc3f22dc56e0a9ac0e556f98ec
Sheila <sheilacupira@gmail.com>
2025-06-13 10:39:25 -0300

    fix type for navigation in Modocozinha

--- src/ModoCozinha.tsx
+++ src/ModoCozinha.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
 import { BotaoCard } from "../assets/components/BotaoCard";
 import { useNavigation } from "@react-navigation/native";
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { RootStackParamList } from './routes/types';
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "./routes/types";
+
+type ModoCozinhaScreenProps =
+  NativeStackNavigationProp<RootStackParamList, "ModoCozinha">;
 
 export function ModoCozinha() {
-  const navigation = useNavigation();
+  const navigation = useNavigation<ModoCozinhaScreenProps>();
 
   return (
     <SafeAreaView style={styles.container}>
